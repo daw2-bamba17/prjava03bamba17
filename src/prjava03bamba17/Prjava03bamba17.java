4f3e1fff5c7f9c262aa930e154a308270993adaf
ubu16 <ubu16@localhost>
2017-02-15 18:19:27 +0100

    Versió 0.4-B del projecte prjava03cognom17. Branca master modificada.

--- src/prjava03bamba17/Prjava03bamba17.java
+++ src/prjava03bamba17/Prjava03bamba17.java
@@ -2,6 +2,7 @@ package prjava03bamba17;
 
 import java.awt.*;        // using AWT containers and components 
 import java.awt.event.*;  // using AWT events and listener interfaces 
+import java.net.*;
  
 // An AWT GUI program inherits the top-level container java.awt.Frame 
 public class Prjava03bamba17 extends Frame 
